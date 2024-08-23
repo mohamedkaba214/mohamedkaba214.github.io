4131ce7906beee4126f724a38103e857ea4c03f2
mohamedkaba214 <94300125+mohamedkaba214@users.noreply.github.com>
2024-08-23 17:19:03 -0400

    Add files via upload

---
+++ Project1/project1.js
@@ -0,0 +1,26 @@
+{/* <script>
+    function openLoginModal() {
+        document.getElementById('loginModal').style.display = 'block'
+    }
+
+    function closeLoginModal() {
+        document.getElementById('loginModal').style.display = 'none'
+    }
+
+    function openTab(evt, tabName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none"
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "")
+        }
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active"
+    }
+
+    // Default to open the Login tab
+    document.getElementsByClassName("tablinks")[0].click()
+</script> */}
